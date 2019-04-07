a8d9eb235128e5ddf96851bdd29726ae610abc86
gxl <geduo_83@163.com>
2019-04-07 22:11:52 +0800

    add INoDataView.java

---
+++ lib_common/src/main/java/com/fly/tour/common/mvp/INoDataView.java
@@ -0,0 +1,15 @@
+package com.zjx.vcars.common.mvp;
+
+/**
+ * Description: <INoDataView><br>
+ * Author:      gxl<br>
+ * Date:        2019/3/11<br>
+ * Version:     V1.0.0<br>
+ * Update:     <br>
+ */
+public interface INoDataView {
+    //显示无数据View
+    void showNoDataView();
+    //隐藏无数据View
+    void hideNoDataView();
+}
