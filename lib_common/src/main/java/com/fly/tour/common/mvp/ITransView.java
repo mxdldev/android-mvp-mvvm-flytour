881ee89ca60613616ec2d4b5ad46fcd73297b519
gxl <geduo_83@163.com>
2019-04-07 22:12:46 +0800

    add ITransView.java

---
+++ lib_common/src/main/java/com/fly/tour/common/mvp/ITransView.java
@@ -0,0 +1,15 @@
+package com.zjx.vcars.common.mvp;
+
+/**
+ * Description: <ITransView><br>
+ * Author:      gxl<br>
+ * Date:        2019/2/26<br>
+ * Version:     V1.0.0<br>
+ * Update:     <br>
+ */
+public interface ITransView {
+    //显示背景透明小菊花View,例如删除操作
+    void showTransLoadingView();
+    //隐藏背景透明小菊花View
+    void hideTransLoadingView();
+}
