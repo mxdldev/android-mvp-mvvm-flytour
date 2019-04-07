6214db9a0d976728671c42440d772f2cdc646a57
gxl <geduo_83@163.com>
2019-04-07 22:09:53 +0800

    add BaseModel

---
+++ lib_common/src/main/java/com/fly/tour/common/mvp/ILoadView.java
@@ -0,0 +1,15 @@
+package com.zjx.vcars.common.mvp;
+
+/**
+ * Description: <ILoadView><br>
+ * Author:      gxl<br>
+ * Date:        2019/2/26<br>
+ * Version:     V1.0.0<br>
+ * Update:     <br>
+ */
+public interface ILoadView {
+    //显示初始加载的View，初始进来加载数据需要显示的View
+    void showInitLoadView();
+    //隐藏初始加载的View
+    void hideInitLoadView();
+}
